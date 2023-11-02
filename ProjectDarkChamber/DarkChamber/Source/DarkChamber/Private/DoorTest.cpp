b23849d979f8f43458b8bb91352a575cc451c80a
Sorrond <rafaelsorrond@gmail.com>
2023-11-02 12:02:54 +0000

    Door test files

---
+++ ProjectDarkChamber/DarkChamber/Source/DarkChamber/Private/DoorTest.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DoorTest.h"
+
+// Sets default values
+ADoorTest::ADoorTest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADoorTest::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADoorTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
