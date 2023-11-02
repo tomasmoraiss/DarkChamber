cd9447e2191644cd2b8aedc75289e6ea76736a12
AkaKoichi <bruninhomartins240@gmail.com>
2023-11-02 10:20:56 +0000

    interface and stoped ignore

---
+++ ProjectDarkChamber/DarkChamber/Source/DarkChamber/Public/Trap.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DarkChamber/InteractInterface.h"
+#include "GameFramework/Actor.h"
+#include "Trap.generated.h"
+
+UCLASS()
+class DARKCHAMBER_API ATrap : public AActor,public IInteractInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATrap();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Interact() override;
+
+};
